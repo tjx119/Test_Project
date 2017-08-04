4a77ff6ad95e062f58b4145118330655d821b933
tjx119 <2006599@qq.com>
2017-08-04 17:48:14 +0800

    8.4 update in office

--- Pandas_ex01.py
+++ Pandas_ex01.py
@@ -1,9 +1,22 @@
-#Note: First array elements raised to powers from second array
 import pandas as pd
 from pandas import DataFrame, Series
 
 movies = pd.read_csv("http://bit.ly/imdbratings")
-#m = movies[movies.duration >= 200]['star_rating']
-m = movies.loc[movies.duration <= 120]
-print m.head()
-print type(m)
+#How do I filter rows of a pandas DataFrame by column value?
+#two methods
+m = movies[movies.duration >= 200]['star_rating']
+m = movies.loc[movies.duration >= 200, 'star_rating']
+#print m.head()
+
+#How do I apply multiple filter criteria to a pandas DataFrame?
+#can not use "and, or", you shoule use "&, |"
+m1 = movies[(movies.duration >= 200) & (movies.star_rating > 8.5)]
+m1 = movies[movies.genre.isin(['Crime', "Drama"]) & (movies.duration >= 200)]
+#print m1.head()
+
+#How do I sort a pandas DataFrame or a Series?
+#This is Series method
+m2 = movies.star_rating.sort_values()
+#This is DataFrame method
+m2 = movies.sort_values('star_rating', ascending=False)
+print m2.head()
