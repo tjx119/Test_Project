d89a3809ae6d1bea217a2bf898a5b4158038c9d9
tjx119 <2006599@qq.com>
2017-08-05 12:26:17 +0800

    test

--- Test.py
+++ Test.py
@@ -1,5 +1,9 @@
-for i in range(3):
-    print i + 1
-print 'Hello'
+# -*- coding: UTF-8 -*-
+import pandas as pd
+from pandas import DataFrame, Series
 
-office commite test
+csv_path = 'e:/Test_Data/2017_08_05-10_40_13.csv'
+
+sales = pd.read_csv(csv_path, encoding='GBK', skiprows=9)
+print sales.head()
+print '的骄傲ID静安寺'
