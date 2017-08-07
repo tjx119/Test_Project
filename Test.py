2e6259034f9de72f08e8142053ea01ffc63c0300
tjx119 <2006599@qq.com>
2017-08-07 19:02:23 +0800

    Update

--- Test.py
+++ Test.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python2
 # -*- coding: UTF-8 -*-
-import pandas as pd
-from pandas import DataFrame, Series
 
-csv_path = 'e:/Test_Data/2017_08_05-10_40_13.csv'
+a = 5
 
-sales = pd.read_csv(csv_path, encoding='GBK', skiprows=9, skipfooter=1)
-print sales.head()
+class func(object):
 
-print sales.sort_values(u'销售数量')
+    def __init__(self):
+        pass
+
+    def add(self):
+        a = 6
+        return a + 3
+
+
+f = func()
+print f.add()
