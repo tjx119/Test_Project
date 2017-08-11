903342f01c5eda108ac2417452d323e1cd971e78
tjx119 <2006599@qq.com>
2017-08-11 23:09:37 +0800

    0

--- Test.py
+++ Test.py
@@ -1,33 +1,25 @@
 #!/usr/bin/env python2
 # -*- coding: UTF-8 -*-
 
-a = 5
-class Fjs(object):
-    def __init__(self, name):
-        self.name = name
-
-    def hello(self):
-        print "said by : ", self.name
-        print a
-    def fjs(self, name):
-        if name == self.name:
-            print "yes"
-        else:
-            print "no"
+from nltk.tkinter import *
+import tkinter as tk
 
 
-class Wrap_Fjs(object):
-    def __init__(self, fjs):
-        self._fjs = fjs
 
-    def __getattr__(self, item):
-        if item == "hello":
-            print "调用hello方法了"
-        elif item == "fjs":
-            print "调用fjs方法了"
-        return getattr(self._fjs, item)
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
 
+    def createWidgets(self):
+        self.helloLabel = Label(self, text='Hello, world!')
+        self.helloLabel.pack()
+        self.quitButton = Button(self, text='Quit', command=self.quit)
+        self.quitButton.pack()
 
-fjs = Wrap_Fjs(Fjs("fjs"))
-fjs.hello()
-fjs.fjs("fjs")
+app = Application()
+# 设置窗口标题:
+app.master.title('Hello World')
+# 主消息循环:
+app.mainloop()
